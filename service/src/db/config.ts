a0541f8fb9af01ba2c5d57b60651418b9b871605
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-04-10 09:44:58 -0700

    db config

---
+++ service/src/db/config.ts
@@ -0,0 +1,16 @@
+
+export interface DBConfig {
+    host: string
+    user: string
+    password: string
+    database: string
+    port: number
+}
+
+export const dbConfig: DBConfig = {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    port: +process.env.DB_PORT!
+}
---
+++ service/src/lib/constants/routes.ts
@@ -0,0 +1,9 @@
+
+const API_LOGIN: string = "/api/login"
+
+const PAGE_LOGIN: string = "/login"
+
+const PUBLIC_ROUTES: string[] = [
+    API_LOGIN,
+    PAGE_LOGIN,
+]
